8059e227de1678e0c05f77ad686ef43b8432e88c
gues <gues@itu.dk>
2022-10-10 19:01:10 +0200

    Added Basic Tests

--- GildedRose.Tests/ProgramTests.cs
+++ GildedRose.Tests/ProgramTests.cs
@@ -3,13 +3,81 @@ namespace GildedRose.Tests;
 public class ProgramTests
 {
     [Theory]
-    [InlineData("Sulfuras, Hand of Ragnaros", -45, 1, 1)]
-    [InlineData("Sulfuras, Hand of Ragnaros", -5, 5, 5)]
-    [InlineData("Sulfuras, Hand of Ragnaros", 0, 10, 10)]
-    [InlineData("Sulfuras, Hand of Ragnaros", 1, 20, 20)]
-    [InlineData("Sulfuras, Hand of Ragnaros", 10, 25, 25)]
-    [InlineData("Sulfuras, Hand of Ragnaros", 15, 40, 40)]
-    public void Given_Sulfuras_Quality_Never_Changes_When_Updating(string item, int sellIn, int quality, int expected)
+    [InlineData("Sword", 3, 4, 3)]
+    [InlineData("Potion", 6, 8, 7)]
+    [InlineData("Spoon", 9, 12, 11)]
+    [InlineData("Glass", 12, 16, 15)]
+    [InlineData("Axe", 15, 20, 19)]
+    public void Given_New_Item_Quality_Drops_By_One_When_Updating(string item, int sellIn, int quality, int expected)
+    {
+        // Arrange
+        Program program = new Program();
+
+        List<Item> items = new ()
+        {
+            new Item{ Name = item, SellIn = sellIn, Quality = quality }
+        };
+        
+        // Act
+        program.UpdateQuality(items);
+
+        // Assert
+        Assert.Equal(expected, items[0].Quality);
+    }
+
+    [Theory]
+    [InlineData("Sword", 3, 4, 2)]
+    [InlineData("Potion", 6, 8, 5)]
+    [InlineData("Spoon", 9, 12, 8)]
+    [InlineData("Glass", 12, 16, 11)]
+    [InlineData("Axe", 15, 20, 14)]
+    public void Given_New_Item_SellIn_Drops_By_One_When_Updating(string item, int sellIn, int quality, int expected)
+    {
+        // Arrange
+        Program program = new Program();
+
+        List<Item> items = new ()
+        {
+            new Item{ Name = item, SellIn = sellIn, Quality = quality }
+        };
+        
+        // Act
+        program.UpdateQuality(items);
+
+        // Assert
+        Assert.Equal(expected, items[0].SellIn);
+    }
+
+    [Theory]
+    [InlineData("Sword", 3, 0, 0)]
+    [InlineData("Potion", 6, 0, 0)]
+    [InlineData("Spoon", 9, 0, 0)]
+    [InlineData("Glass", 12, 0, 0)]
+    [InlineData("Axe", 15, 0, 0)]
+    public void Given_New_Item_Quality_Wont_Drop_Below_Zero(string item, int sellIn, int quality, int expected)
+    {
+        // Arrange
+        Program program = new Program();
+
+        List<Item> items = new ()
+        {
+            new Item{ Name = item, SellIn = sellIn, Quality = quality }
+        };
+        
+        // Act
+        program.UpdateQuality(items);
+
+        // Assert
+        Assert.Equal(expected, items[0].Quality);
+    }
+
+    [Theory]
+    [InlineData("Sword", 0, 4, 2)]
+    [InlineData("Potion", -6, 8, 6)]
+    [InlineData("Spoon", -9, 12, 10)]
+    [InlineData("Glass", -12, 16, 14)]
+    [InlineData("Axe", -15, 20, 18)]
+    public void Given_New_Item_Quality_Drops_Twice_As_Fast_When_SellIn_Is_Zero_Or_Less(string item, int sellIn, int quality, int expected)
     {
         // Arrange
         Program program = new Program();
